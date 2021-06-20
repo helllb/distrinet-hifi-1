09cf1915546fed015b6af3362a91ecfb3b6e8101
distrinet-hifi <86104961+distrinet-hifi@users.noreply.github.com>
2021-06-20 19:23:15 +0200

    Update compile.sh

--- distrinet-hifi/compile.sh
+++ distrinet-hifi/compile.sh
@@ -1,6 +1,7 @@
 netns=$1
 dev=$2
 rate=$3
+delay=$4
 
 
 
@@ -11,7 +12,8 @@ ip netns exec $netns tc qdisc del dev $dev root
 
 ip netns exec $netns tc qdisc add dev $dev root handle 1: htb
 ip netns exec $netns tc class add dev $dev parent 1: classid 1:10 htb rate $rate
-ip netns exec $netns tc filter add dev $dev parent 1: bpf da obj /root/project/foo sec egress flowid 1:10
+ip netns exec $netns tc filter add dev $dev parent 1: bpf da obj /root/experiment/foo sec egress flowid 1:10
+ip netns exec $netns tc qdisc add dev $dev parent 1:10 handle 2: netem delay $delay
 
 ip netns exec $netns tc qdisc add dev $dev ingress handle ffff:
-ip netns exec $netns tc filter add dev $dev parent ffff: bpf da obj /root/project/foo sec ingress
+ip netns exec $netns tc filter add dev $dev parent ffff: bpf da obj /root/experiment/foo sec ingress
